48c68f5978220aaacd1859a8ae8e42cc0ea3253e
Abhishek Bondre <abhibondre27@gmail.com>
2023-09-19 20:11:09 +0530

    modify

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -20,7 +20,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../animation/typing.json";
 
-const ENDPOINT = "https://chat-app-j7sk.onrender.com";
+const ENDPOINT = "http://localhost:4000";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
